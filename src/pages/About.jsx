709d31a4bdbfd5fcb66c73f2dc4960d954f6309f
Falcho <m.k.falcham@gmail.com>
2025-05-16 11:09:50 +0200

    Made some pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About this app</h1>
+      <p>
+        This app is designed to help teachers manage and distribute math
+        problems provided by the education department. All problems are imported
+        into the app, sorted, and organized for easy access. As a teacher, you
+        can create teams for each of your classes and assign them specific
+        problems. The app keeps track of which problems have already been given
+        to each team, helping you avoid repetition and ensuring a smoother
+        planning process. Whether you're managing one class or several, this
+        tool simplifies your workflow and supports more effective teaching.
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/Ideas.jsx
@@ -0,0 +1,8 @@
+export default function ideas() {
+  return (
+    <div>
+      <h1>Ideas</h1>
+      <p>This is the ideas page of our application.</p>
+    </div>
+  );
+}
