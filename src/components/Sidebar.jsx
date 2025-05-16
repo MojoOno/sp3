b18aa5da656cd45d4ac13edb68212732ce91256b
AndyTheDragon <andre.jansam@gmail.com>
2025-05-16 10:31:49 +0200

    work on sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,54 @@
 import { NavLink } from "react-router-dom";
+import styles from "../css/Sidebar.module.css";
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <h2>Sidebar</h2>
+    <div className={styles.sidebar}>
+      <h2>Menu</h2>
       <ul>
-        <li><NavLink to="about">About</NavLink></li>
-        <li><NavLink to="endpoints">Endpoints</NavLink></li>
-        <li><NavLink to="deployed">Deployed</NavLink></li>
-        <li><NavLink to="repository">Repository</NavLink></li>
-        <li><NavLink to="ideas">Ideas</NavLink></li>
+        <li>
+          <NavLink 
+            to="about" 
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to="endpoints" 
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Endpoints
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to="deployed" 
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Deployed
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to="repository" 
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Repository
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to="ideas" 
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Ideas
+          </NavLink>
+        </li>
       </ul>
     </div>
   );
-}
+};
+
 export default Sidebar;
