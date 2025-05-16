03e64bb308136001182aed176e3e24681fcc9f41
AndyTheDragon <andre.jansam@gmail.com>
2025-05-16 10:01:50 +0200

    Updated sidebar to include the NavLinks

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <h2>Sidebar</h2>
       <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
+        <li><NavLink to="about">About</NavLink></li>
+        <li><NavLink to="endpoints">Endpoints</NavLink></li>
+        <li><NavLink to="deployed">Deployed</NavLink></li>
+        <li><NavLink to="repository">Repository</NavLink></li>
+        <li><NavLink to="ideas">Ideas</NavLink></li>
       </ul>
     </div>
   );
